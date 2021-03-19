b73f29b74f3994842218b2791a371346d0fa3416
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-03-18 23:36:28 -0400

    cyanide and happiness cartoon command

---
+++ src/Commands/Fun/Cartoons/CyanideAndHappiness.ts
@@ -0,0 +1,70 @@
+import { Message } from 'discord.js';
+import { decodeXML } from 'entities';
+import { cahTransaction, migrateCAH } from '../../../lib/Migration/CyanideAndHappiness.js';
+import { isText } from '../../../lib/types/Discord.js.js';
+import { RSSReader } from '../../../lib/Utility/RSS.js';
+import { Command } from '../../../Structures/Command.js';
+import { pool } from '../../../Structures/Database/Postgres.js';
+import { RegisterCommand } from '../../../Structures/Decorator.js';
+
+interface ICyanideAndHappiness {
+    title: string
+    link: string
+    description: string
+    category: string
+    guid: string
+    pubDate: string
+}
+
+interface Comic {
+    comic_key: number
+    href: string
+    link: string
+    title: string
+}
+
+const rss = new RSSReader<ICyanideAndHappiness>(async () => {
+    const comics = [...rss.results.values()].map(item => ({
+        href: item.link,
+        title: decodeXML(item.title),
+        link: `https:${item.description.match(/src="(.*?)"/)[1]}`
+    }));
+
+    await cahTransaction(comics);
+});
+// https://github.com/daniellowtw/explosm-rss
+// does the scraping for us, so might as well use until it's no longer available
+rss.cache('https://explosm-1311.appspot.com/');
+
+@RegisterCommand
+export class kCommand extends Command {
+    middleware = [migrateCAH];
+    constructor() {
+        super(
+            [
+                'Get a random comic from Cyanide and Happiness! Possibly NSFW (18+).'
+            ],
+			{
+                name: 'cyanideandhappiness',
+                folder: 'Games',
+                args: [0, 0],
+                aliases: ['cah']
+            }
+        );
+    }
+
+    async init(message: Message) {
+        if (isText(message.channel) && !message.channel.nsfw) {
+            return this.Embed.fail('Channel isn\'t marked as NSFW!');
+        }
+
+        const { rows } = await pool.query<Comic>(`
+            SELECT * FROM kbCAH TABLESAMPLE BERNOULLI(1) ORDER BY random() LIMIT 1;
+        `);
+
+        return this.Embed.success()
+            .setTitle(rows[0].title)
+            .setURL(rows[0].href)
+            .setImage(rows[0].link);
+    }
+}
---
+++ src/lib/Migration/CyanideAndHappiness.ts
@@ -0,0 +1,45 @@
+import { readFile } from 'fs/promises';
+import { join } from 'path';
+import { pool } from '../../Structures/Database/Postgres.js';
+
+interface Comic {
+    href: string
+    link: string
+    title: string
+}
+
+const PATH = join(process.cwd(), 'assets/CyanideAndHappiness.json');
+
+export const migrateCAH = async () => {
+    const { rows } = await pool.query<{ exists: boolean }>(`SELECT EXISTS(SELECT 1 FROM kbCAH);`);
+    if (rows[0].exists === false) {
+        const file = JSON.parse(await readFile(PATH, 'utf-8')) as Comic[];
+        await cahTransaction(file);
+    }
+
+    return true;
+}
+
+export const cahTransaction = async (comics: Comic[]) => {
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+
+        for (const comic of comics) {
+            await client.query(`
+                INSERT INTO kbCAH (
+                    href, link, title
+                ) VALUES (
+                    $1, $2, $3
+                ) ON CONFLICT DO NOTHING;
+            `, [comic.href, comic.link, comic.title]);
+        }
+
+        await client.query('COMMIT');
+    } catch {
+        await client.query('ROLLBACK');
+    } finally {
+        client.release();
+    }
+}
