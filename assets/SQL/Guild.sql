0a43e7a8d7555b877ad008a19f3bbd4f95b46b5c
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-05-09 22:02:12 -0400

    WIP new rule commands

--- assets/SQL/Guild.sql
+++ assets/SQL/Guild.sql
@@ -1,3 +1,4 @@
+DROP TABLE kbRules;
 DROP TABLE kbWarns;
 DROP TABLE kbGuild;
 
@@ -8,6 +9,7 @@ CREATE TABLE IF NOT EXISTS kbGuild (
     max_warning_points SMALLINT DEFAULT 20,
     mod_log_channel TEXT DEFAULT NULL,
     welcome_channel TEXT DEFAULT NULL,
+    rules_channel TEXT DEFAULT NULL,
     UNIQUE (guild_id)
 );
 
@@ -20,10 +22,17 @@ CREATE TABLE IF NOT EXISTS kbWarns (
     FOREIGN KEY (k_guild_id) REFERENCES kbGuild(guild_id) ON DELETE CASCADE
 );
 
+CREATE TABLE IF NOT EXISTS kbRules (
+    id SERIAL PRIMARY KEY,
+    k_guild_id TEXT NOT NULL,
+    rule TEXT NOT NULL,
+    FOREIGN KEY (k_guild_id) REFERENCES kbGuild(guild_id) ON DELETE CASCADE
+);
+
 INSERT INTO kbGuild (
-    guild_id, max_warning_points
+    guild_id, max_warning_points, rules_channel
 ) VALUES (
-    '503024525076725771', 20
+    '503024525076725771', 20, '688943609348882456'
 ) ON CONFLICT DO NOTHING;
 
 INSERT INTO kbWarns (
@@ -36,4 +45,16 @@ INSERT INTO kbWarns (
     k_guild_id, k_user_id, k_points
 ) VALUES (
     '503024525076725771', '267774648622645249', 4
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO kbRules (
+    k_guild_id, rule
+) VALUES (
+    '503024525076725771', 'Dont be a dick sucker'
+) ON CONFLICT DO NOTHING;
+
+INSERT INTO kbRules (
+    k_guild_id, rule
+) VALUES (
+    '503024525076725771', 'Dont be a cuck'
 ) ON CONFLICT DO NOTHING;
--- src/Commands/Moderation/Rules/Post.ts
+++ src/Commands/Moderation/Rules/Post.ts
@@ -1,14 +1,17 @@
 import { Command, Arguments } from '../../../Structures/Command.js';
-import { 
-    Message, 
-    Permissions, 
-    TextChannel
-} from 'discord.js';
-import { GuildSettings } from '../../../lib/types/Collections.js';
+import { Message, Permissions } from 'discord.js';
+import { RegisterCommand } from '../../../Structures/Decorator.js';
+import { pool } from '../../../Structures/Database/Postgres.js';
 import { isText } from '../../../lib/types/Discord.js.js';
 import { hasPerms } from '../../../lib/Utility/Permissions.js';
-import { RegisterCommand } from '../../../Structures/Decorator.js';
-import { delay } from '../../../lib/Utility/Constants/OneLiners.js';
+import { chunkSafe } from '../../../lib/Utility/Array.js';
+
+const perms = new Permissions([
+    Permissions.FLAGS.SEND_MESSAGES,
+    Permissions.FLAGS.EMBED_LINKS,
+    Permissions.FLAGS.READ_MESSAGE_HISTORY,
+    Permissions.FLAGS.MANAGE_CHANNELS
+]);
 
 @RegisterCommand
 export class kCommand extends Command {
@@ -22,38 +25,56 @@ export class kCommand extends Command {
                 aliases: [ 'postrules', 'postrule' ],
                 folder: 'Rules',
                 args: [0, 0],
-                guildOnly: true
+                guildOnly: true,
+                ratelimit: 60
             }
         );
     }
 
-    async init(message: Message, _args: Arguments, settings: GuildSettings) {
-        if (!hasPerms(message.channel, message.member, Permissions.FLAGS.ADMINISTRATOR)) {
-            return this.Embed.missing_perms(true);
-        } else if (!settings || !('rules' in settings) || !settings.rules.rules?.length) {
+    async init(message: Message, _args: Arguments) {
+        const { rows } = await pool.query<{ rules_channel: string | null, rule: string }>(`
+            SELECT rules_channel, rule FROM kbGuild
+            INNER JOIN kbRules ON kbGuild.guild_id = kbRules.k_guild_id
+            WHERE kbGuild.guild_id = $1::text;
+        `, [message.guild.id]);
+
+        if (rows[0].rules_channel === null) // rule channel not set
             return this.Embed.fail(`
-            Guild has no rules.
+            No \`rules\` channel set in this guild. 
 
-            Use the \`\`rules\`\` command to get started!
+            Add a channel using \`rulechannel\` before using this command! 🤗
             `);
-        }
+        else if (!message.guild.channels.cache.has(rows[0].rules_channel)) // deleted, no access, etc.
+            return this.Embed.fail(`
+            Channel was deleted or I no longer have access to it. 😕
 
-        const channel = message.guild.channels.cache.get(settings.rules.channel) as TextChannel;
-        if (!channel || !isText(channel)) {
-            return this.Embed.fail(`Channel ${channel ?? settings.rules.channel} is invalid!`);
-        } else if (!hasPerms(channel, message.guild.me, ['EMBED_LINKS', 'SEND_MESSAGES'])) {
-            return this.Embed.fail(`Missing permissions to either use embeds or send messages!`);
-        }
+            Try changing the channel using the command \`rulechannel\`!
+            `);
 
-        await message.reply(this.Embed.success(`
-        Posting ${settings.rules.rules.length} rules in intervals of 5 seconds.
-        `));
+        const channel = message.guild.channels.cache.get(rows[0].rules_channel);
 
-        for (const rule of settings.rules.rules) {
-            await channel.send(this.Embed.success(`
-            ${rule.rule}
-            `).setTitle(`Rule ${rule.index}`));
-            await delay(5000);
-        }
+        if (!isText(channel)) // not possible but ts will not infer textchannel | newschannel, plus might as well be safe
+            return this.Embed.fail('You did the impossible. Change the channel (\`rulechannel\`) please.');
+        else if (!hasPerms(channel, message.guild.me, perms)) // doesn't have perms to post
+            return this.Embed.fail('Give me permissions to read message history, embed links, manage channel, and send messages please!');
+
+        await message.reply(`Posting ${rows.length} rules to ${channel} now!`);
+        
+        const embeds = rows.map((rule, idx) => this.Embed.success(`${rule.rule}`).setTitle(`Rule #${idx+1}`));
+        if (embeds.length === 1)
+            return channel.send(embeds[0]);
+
+        const groups = chunkSafe(embeds, 10); // max # of embeds 1 webhook msg can have
+        const webhooks = await channel.fetchWebhooks(); // check for existing webhooks
+
+        const webhook = webhooks.size === 0 
+            ? await channel.createWebhook('Khafra-Bot Rules', { // no webhook, create one
+                avatar: message.client.user.displayAvatarURL(),
+                reason: 'posting rules'
+              })
+            : webhooks.find(wh => wh.type === 'Incoming'); // don't want channel following
+
+        for (const group of groups)
+            await webhook.send({ embeds: group });
     }
 }
