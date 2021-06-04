5d4bbd984f8ff9208459b08c24dcf0ad8147c965
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-06-04 19:41:22 -0400

    create global slash commands

--- src/Structures/Decorator.ts
+++ src/Structures/Decorator.ts
@@ -31,10 +31,13 @@ export const RegisterInteraction = <T extends new (...args: unknown[]) => Intera
 ) => {
     const interaction = new InteractionObject();
 
-    // TODO(@KhafraDev): set this as a global slash command. 
+    client.application.commands.create(interaction.data)
+        .catch(() => {});
+
     // There isn't a better way to check if a bot can make a slash command "yet".
     // https://discord.com/channels/222078108977594368/824410868505903114/848343604308475934
     client.guilds.cache.get('503024525076725771').commands.create(interaction.data)
         .catch(() => {});
+
     KhafraClient.Interactions.set(interaction.data.name, interaction);
 }
