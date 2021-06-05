d467c7c6493afb28dfcf1e1771ed9972b07f39b0
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-06-05 12:18:04 -0400

    add owner only `eval` command

---
+++ src/Commands/Bot/Eval.ts
@@ -0,0 +1,41 @@
+import { Message } from 'discord.js';
+import { inspect } from 'util';
+import { compileFunction } from 'vm';
+import { Command, Arguments } from '../../Structures/Command.js';
+import { RegisterCommand } from '../../Structures/Decorator.js';
+
+@RegisterCommand
+export class kCommand extends Command {
+    constructor() {
+        super([
+            'Evaluate a statement.'
+        ], {
+            name: 'eval',
+            folder: 'Bot',
+            args: [0],
+            ratelimit: 0,
+            ownerOnly: true
+        });
+    }
+
+    async init(message: Message, { content }: Arguments) {
+        // https://github.com/nodejs/node/blob/e46c680bf2b211bbd52cf959ca17ee98c7f657f5/test/parallel/test-vm-basic.js#L132-L145
+
+        const fn = compileFunction(
+            `${content}`,
+            ['message']
+        ) as (...params: unknown[]) => unknown;
+
+        let ret: unknown;
+        try {
+            ret = fn(message);
+        } catch (e) {
+            ret = e;
+        }
+
+        const text = inspect(ret, true, 1, false);
+        return this.Embed.success(`
+        \`\`\`js\n${text.slice(0, 2004).trim()}\`\`\`
+        `);
+    }
+}
