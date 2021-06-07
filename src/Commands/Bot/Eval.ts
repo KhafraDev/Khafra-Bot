d25c96d2c65eb86a59d615767ec30dd3d08d4800
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-06-07 01:14:17 -0400

    patch eval

--- src/Commands/Bot/Eval.ts
+++ src/Commands/Bot/Eval.ts
@@ -21,6 +21,9 @@ export class kCommand extends Command {
     async init(message: Message, { content }: Arguments) {
         // https://github.com/nodejs/node/blob/e46c680bf2b211bbd52cf959ca17ee98c7f657f5/test/parallel/test-vm-basic.js#L132-L145
 
+        if (content.includes('import'))
+            return this.Embed.fail('Import isn\'t supported yet.');
+
         let ret: unknown;
         try {
             // vm.compileFunction can throw when using unsupported syntax
