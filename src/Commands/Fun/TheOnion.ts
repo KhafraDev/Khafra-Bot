85c31a5061d3c229d31042916aa6b9082e7194c2
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-06-09 22:06:50 -0400

    update `theonion` command
    - IDs are no longer stored in a JSON file (the file was never actually updated anyways)
    - parses RSS feed and gets the article from there instead now.

--- src/Commands/Fun/TheOnion.ts
+++ src/Commands/Fun/TheOnion.ts
@@ -1,15 +1,30 @@
 import { Command } from '../../Structures/Command.js';
-import TheOnion from '../../../assets/TheOnion.json';
 import { decodeXML } from 'entities';
 import { RegisterCommand } from '../../Structures/Decorator.js';
 import { fetch } from '../../Structures/Fetcher.js';
+import { RSSReader } from '../../lib/Utility/RSS.js';
+import { once } from '../../lib/Utility/Memoize.js';
+import { rand } from '../../lib/Utility/Constants/OneLiners.js';
+
+interface ITheOnion {
+    title: string
+    link: string
+    description: string
+    category: string[]
+    pubDate: string
+    guid: number
+    'dc:creator': string
+}
+
+const rss = new RSSReader<ITheOnion>();
+const cache = once(() => rss.cache(`https://www.theonion.com/rss`));
 
 @RegisterCommand
 export class kCommand extends Command {
     constructor() {
         super(
             [ 
-                'Read an article from TheOnion!',
+                'Read one of the latest articles from The Onion!',
                 ''
             ],
 			{
@@ -22,7 +37,10 @@ export class kCommand extends Command {
     }
 
     async init() {
-        const id = TheOnion[Math.floor(Math.random() * TheOnion.length)];
+        await cache();
+
+        const i = await rand(rss.results.size);
+        const id = [...rss.results][i].guid;
 
         const j = await fetch(`https://theonion.com/api/core/corepost/getList?id=${id}`).json();
 
@@ -33,7 +51,11 @@ export class kCommand extends Command {
             `);
 
         return this.Embed.success()
-            .setAuthor(decodeXML(j.data[0].headline).slice(0, 256), undefined, j.data[0].permalink)
+            .setAuthor(
+                decodeXML(j.data[0].headline).slice(0, 256), 
+                'https://arc-anglerfish-arc2-prod-tronc.s3.amazonaws.com/public/3ED55FMQGXT2OG4GOBTP64LCYU.JPG',
+                j.data[0].permalink
+            )
             .setTimestamp(j.data[0].publishTimeMillis)
             .setDescription(j.data[0].plaintext.slice(0, 2048));
     }
--- src/lib/Utility/Constants/OneLiners.ts
+++ src/lib/Utility/Constants/OneLiners.ts
@@ -2,4 +2,10 @@ import { randomInt } from 'crypto';
 import { promisify } from 'util';
 
 export const delay = promisify(setTimeout);
+/**
+ * Generate a random number!
+ *  
+ * If no second value is provided, the range will be [0, a)
+ * Otherwise, the range will be from [a, b)
+ */
 export const rand: (a: number, b?: number) => Promise<number> = promisify(randomInt);
