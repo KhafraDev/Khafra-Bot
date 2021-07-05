792f7713b17dc1e32fa7508db2de6dd87a22385e
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-07-05 16:22:29 -0400

    add `hacks:edit` command, move ghostping

---
+++ src/Commands/Debug/Glitches/Edit.ts
@@ -0,0 +1,34 @@
+import { Command } from '../../../Structures/Command.js';
+import { Message } from 'discord.js';
+import { RegisterCommand } from '../../../Structures/Decorator.js';
+
+const symbol = String.fromCodePoint(Number.parseInt('202B', 16));
+const clean = 'The edit button won\'t be where you expect it to be!';
+const edited = `The edit button is right there -> ${symbol} !Over here ->`;
+
+@RegisterCommand
+export class kCommand extends Command {
+    constructor() {
+        super(
+            [
+                'Place an edit button in the middle of a message!'
+            ],
+            {
+                name: 'hacks:edit',
+                folder: 'Debug',
+                args: [0, 0],
+                ratelimit: 3
+            }
+        );
+    }
+
+    async init(message: Message) {
+        const m = await message.reply({ content: clean });
+
+        if (m.editable) {
+            return m.edit({ content: edited });
+        } else {
+            return this.Embed.fail(`Message wasn\'t editable!`);
+        }
+    }
+}
