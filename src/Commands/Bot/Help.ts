ccace9299af6d48dc9de89084278827766e5246d
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-07-13 16:45:10 -0400

    improve `help` command

--- src/Commands/Bot/Help.ts
+++ src/Commands/Bot/Help.ts
@@ -1,9 +1,13 @@
-import { Command, Arguments } from '../../Structures/Command.js';
-import { Message } from 'discord.js';
-import { KhafraClient } from '../../Bot/KhafraBot.js';
-import { compareTwoStrings } from '../../lib/Utility/CompareStrings.js';
-import { upperCase } from '../../lib/Utility/String.js';
+import { Arguments, Command } from '../../Structures/Command.js';
+import { Message, MessageActionRow, MessageEmbed, MessageSelectMenu } from 'discord.js';
 import { RegisterCommand } from '../../Structures/Decorator.js';
+import { KhafraClient } from '../../Bot/KhafraBot.js';
+import { chunkSafe } from '../../lib/Utility/Array.js';
+import { bold, inlineCode, hyperlink } from '@discordjs/builders';
+import { Components, disableAll } from '../../lib/Utility/Constants/Components.js';
+import { kGuild } from '../../lib/types/KhafraBot.js';
+
+let folders: string[] | null = null;
 
 @RegisterCommand
 export class kCommand extends Command {
@@ -23,65 +27,99 @@ export class kCommand extends Command {
         );
     }
 
-    async init(_message: Message, { args }: Arguments) {
-        const folders = [...new Set([...KhafraClient.Commands.values()].map(c => c.settings.folder))];
-        const name = args.join(' ').toLowerCase();
-        const isFolder = folders.some(f => f.toLowerCase() === name);
+    async init(message: Message, _: Arguments, settings: kGuild) {
+        folders ??= [...new Set([...KhafraClient.Commands.values()].map(c => c.settings.folder))];
 
-        if (!KhafraClient.Commands.has(name) && !isFolder) {
-            const all = args.length === 0 ? [] : [...new Set(KhafraClient.Commands.values())]
-                .map(c => [c.settings.name, compareTwoStrings(name, c.settings.name)])
-                .sort((a, b) => +b[1] - +a[1])
-                .slice(0, 5);
-            
-            if (all.every(c => c[1] === 0)) {
-                return this.Embed.success(`
-                [Khafra-Bot](https://github.com/khafradev/khafra-bot)
+        const m = await message.channel.send({
+            embeds: [
+                this.Embed.success(`
+                ${hyperlink('Khafra-Bot', 'https://github.com/KhafraDev/Khafra-Bot')}
+                
+                To get help on a single command use ${inlineCode(`${settings.prefix}help [command name]`)}!
+                `)
+            ],
+            components: [
+                new MessageActionRow().addComponents(
+                    new MessageSelectMenu()
+                        .setCustomId('help')
+                        .setPlaceholder('Select a category of commands!')
+                        .addOptions(folders.map(f => ({
+                            label: f,
+                            description: `Select the ${f} category!`,
+                            value: f
+                        })))
+                )
+            ]
+        });
 
-                Khafra-Bot has a system of sorting commands by \`\`folder\`\`, or a tag that identifies what their function is.
-                To list all the commands in a certain folder, you can use the \`\`help [folder]\`\` command (where \`\`[folder]\`\` refers to the folder's name).
+        let pages: MessageEmbed[] = [],
+            page = 0;
 
-                You can also get help on a single command using \`\`help [command]\`\`!
+        const c = m.createMessageComponentCollector({
+            time: 60_000,
+            max: 10,
+            filter: (interaction) =>
+                interaction.user.id === message.author.id
+        });
 
-                All of the command folders:
-                ${folders.map(f => `\`\`${f}\`\``).join(', ')}
-                `);
+        c.on('collect', (i) => {
+            if (i.isSelectMenu()) {
+                const category = i.values[0];
+                if (!folders.includes(category)) return;
+
+                pages = [];
+                page = 0;
+                const all: Command[] = [];
+                
+                for (const command of KhafraClient.Commands.values()) {
+                    if (all.includes(command)) continue;
+                    if (command.settings.folder !== category) continue;
+
+                    all.push(command);
+                }
+
+                for (const chunk of chunkSafe(all, 20)) {
+                    let desc = '';
+                    for (const { settings, help } of chunk) {
+                        desc += `${bold(settings.name)}: ${inlineCode(help[0].slice(0, 190 - settings.name.length))}\n`;
+                    }
+
+                    pages.push(this.Embed.success(desc));
+                }
+
+                const components: MessageActionRow[] = [];
+                if (pages.length > 1) {
+                    components.push(
+                        new MessageActionRow().addComponents(
+                            Components.deny('Previous', 'previous'),
+                            Components.approve('Next', 'next'),
+                            Components.secondary('Stop', 'stop')
+                        )
+                    );
+                }
+
+                if (m.components.length === 1)
+                    components.push(...m.components);
+                else
+                    components.push(m.components[m.components.length - 1]);
+
+                console.log(pages[page], page, pages);
+                return i.update({ 
+                    embeds: [pages[page]],
+                    components
+                });
             } else {
-                return this.Embed.success(`
-                No command with that name was found, however some commands may have a similar name. 
-                Commands with similar names: 
-                ${all.map(n => `\`\`${n[0]}\`\` (${(+n[1] * 100).toFixed(2)}% similarity)`).join('\n')}
-                `);
+                if (i.customId === 'stop') {
+                    c.stop();
+                    return i.update({ components: disableAll(m) });
+                } else if (i.customId === 'previous') {
+                    page = --page < 0 ? pages.length - 1 : page;
+                } else {
+                    page = ++page >= pages.length ? 0 : page;
+                }
+
+                return i.update({ embeds: [pages[page]] });
             }
-        } else if (isFolder) {
-            const cmdsOfFolder = [...KhafraClient.Commands.entries()].filter(([n, v]) => {
-                return !v.settings.aliases.includes(n) && v.settings.folder.toLowerCase() === name;
-            }).map(l => l[0]);
-
-            return this.Embed.success(`
-            ${upperCase(name)} Commands (${cmdsOfFolder.length})
-            ${cmdsOfFolder.map(n => `\`\`${n}\`\``).join(', ')}
-
-            Want to view a single command's info? Use this command again, but provide the name of a command.
-            For example, \`\`help ${cmdsOfFolder[Math.floor(Math.random() * cmdsOfFolder.length)]}\`\`.
-            `);
-        }
-
-        const { settings, help } = KhafraClient.Commands.get(name);
-
-        return this.Embed.success(`
-        The \`\`${settings.name}\`\` command:
-        \`\`\`${help.shift()}\`\`\`
-
-        Aliases: ${settings.aliases.map(a => `\`\`${a}\`\``).join(', ')}
-
-        Example(s):
-        ${help.map(c => `\`\`${settings.name} ${c || '​'}\`\``.trim()).join('\n')}
-        `)
-        .addFields(
-            { name: '**Guild Only:**', value: settings.guildOnly ? 'Yes' : 'No', inline: true },
-            { name: '**Owner Only:**', value: settings.ownerOnly ? 'Yes' : 'No', inline: true },
-            { name: '**Rate-Limit:**', value: `${settings.ratelimit} seconds`, inline: true}
-        );
+        });
     }
 }
