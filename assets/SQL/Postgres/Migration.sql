0e387e3920f80529ffae0503c0593b8efe04dd1f
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-07-14 01:06:11 -0400

    functioning kbWarns migration

--- assets/SQL/Postgres/Migration.sql
+++ assets/SQL/Postgres/Migration.sql
@@ -6,3 +6,14 @@ DROP TABLE IF EXISTS kbGarrison;
 DROP TABLE IF EXISTS kbStonewall;
 
 ALTER TABLE kbGuild ADD COLUMN IF NOT EXISTS complete_log_channel TEXT DEFAULT NULL;
+
+DO
+$do$
+BEGIN
+    IF (SELECT TRUE FROM information_schema.columns WHERE table_name='kbwarns' AND column_name='k_id') THEN
+        ALTER TABLE kbWarns DROP COLUMN IF EXISTS k_id;
+        ALTER TABLE kbWarns DROP COLUMN id;
+        ALTER TABLE kbWarns ADD COLUMN id uuid DEFAULT uuid_generate_v4();
+    END IF;
+END
+$do$
