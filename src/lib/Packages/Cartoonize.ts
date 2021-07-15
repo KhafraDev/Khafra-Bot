24d4dd7d17c29672102914322afdf0e765cb99ee
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-07-14 23:32:23 -0400

    https.request: add agent + AbortController

--- src/lib/Packages/Cartoonize.ts
+++ src/lib/Packages/Cartoonize.ts
@@ -1,33 +1,37 @@
 import FormData from '@discordjs/form-data';
 import { MessageAttachment } from 'discord.js';
 import { decodeXML } from 'entities';
-import https from 'https';
+import { request, Agent } from 'https';
 import { lookup } from 'mime-types';
 import fetch from 'undici-fetch';
 import { URL } from 'url';
 
 /*** Get the image from the html */
 const R = /<div class="image">\s+<img src="(.*?)">/;
+const agent = new Agent({ keepAlive: true });
 
 /**
  * Cartoonize an image using AI from an "unofficial API".
  * Involves HTML scraping.
- * @throws {TypeError | AssertionError | FetchError}
  */
 export const cartoonize = async (attachment: MessageAttachment) => {
-    const m = `${lookup(attachment.name)}`;
-    const r = await fetch(attachment.proxyURL);
     const f = new FormData();
-
+    const a = new AbortController();
+    const r = await fetch(attachment.proxyURL);
+    const m = `${lookup(attachment.name)}`;
+    
+    setTimeout(() => a.abort(), 60000).unref();
     f.append('image', await r.arrayBuffer(), {
         filename: attachment.name,
         contentType: m
     });
 
     return new Promise<string>((done, reject) => {
-        const request = https.request(new URL('https://cartoonize-lkqov62dia-de.a.run.app/cartoonize'), {
+        const req = request(new URL('https://cartoonize-lkqov62dia-de.a.run.app/cartoonize'), {
             method: 'POST',
-            headers: f.getHeaders()
+            headers: f.getHeaders(),
+            agent,
+            abort: a.signal
         }, async (res) => {
             let data = '';
             
@@ -38,9 +42,10 @@ export const cartoonize = async (attachment: MessageAttachment) => {
             return done(decodeXML(R.exec(data)[1]));
         });
         
-        f.pipe(request);
+        f.pipe(req);
         
-        request.end();
-        request.once('error', reject);
+        req.end();
+        req.once('error', reject);
+        req.once('abort', reject);
     });
 }
