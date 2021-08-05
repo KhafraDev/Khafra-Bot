abd88e981a7f82e1662cbd25862775a703d1899b
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-08-05 18:58:41 -0400

    `piglatin`: fix punctuation

--- src/Commands/Fun/Text/PigLatin.ts
+++ src/Commands/Fun/Text/PigLatin.ts
@@ -5,9 +5,29 @@ import { RegisterCommand } from '../../../Structures/Decorator.js';
 const consonants = ['B', 'C', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'X', 'Y', 'Z'];
 const vowels = ['A', 'E', 'I', 'O', 'U'];
 
+const splitWord = (word: string) => {
+    const punc = {
+        // alphanumeric
+        an: [] as string[],
+        // punctuation
+        p: [] as string[]
+    };
+
+    for (let i = 0; i < word.length; i++) {
+        const letter = word[i].toUpperCase();
+        if (consonants.includes(letter) || vowels.includes(letter) || !Number.isNaN(Number(letter))) {
+            punc.an.push(word[i]);
+        } else {
+            punc.p.push(word[i]);
+        }
+    }
+
+    return punc;
+}
+
 const toPigLatin = (sentence: string) => {
     const words = sentence.split(/\s+/g);
-    const pigLatin: string[] = [];
+    const pigLatin = [];
 
     for (const word of words) {
         const start = word.charAt(0).toUpperCase();
@@ -24,15 +44,20 @@ const toPigLatin = (sentence: string) => {
                     }
                 }
 
-                pigLatin.push(`${word.slice(consonantsStart)}${word.slice(0, consonantsStart)}ay`);
+                const front = splitWord(word.slice(consonantsStart));
+                const back = splitWord(word.slice(0, consonantsStart));
+                pigLatin.push(`${front.an.join('')}${back.an.join('')}ay${front.p.join('')}${back.p.join('')}`)
+                // pigLatin.push(`${word.slice(consonantsStart)}${word.slice(0, consonantsStart)}ay`);
             } else {
                 // For words that begin with consonant sounds, all letters before the initial 
                 // vowel are placed at the end of the word sequence. Then, "ay" is added
-                pigLatin.push(`${word.slice(1)}${word[0]}ay`)
+                const { an, p } = splitWord(word);
+                pigLatin.push(`${an.slice(1).join('')}${an[0]}ay${p.join('')}`)
             }
         } else if (vowels.includes(start)) {
             // For words that begin with vowel sounds, the vowel is left alone, and most commonly 'yay' is added to the end.
-            pigLatin.push(`${word}yay`);
+            const { an, p } = splitWord(word);
+            pigLatin.push(`${an.join('')}yay${p.join('')}`);
         } else {
             // symbols, etc.
             pigLatin.push(word);
