b6f99a1af80359a0044f2794880b3bd42ccfb610
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-09-01 18:00:07 -0400

    `ban`: allow passing cli args

--- src/Commands/Moderation/Ban.ts
+++ src/Commands/Moderation/Ban.ts
@@ -8,8 +8,12 @@ import { bans } from '../../lib/Cache/Bans.js';
 import { Range } from '../../lib/Utility/Range.js';
 import { validateNumber } from '../../lib/Utility/Valid/Number.js';
 import { Message } from '../../lib/types/Discord.js.js';
+import { dontThrow } from '../../lib/Utility/Don\'tThrow.js';
+import { inlineCode } from '@discordjs/builders';
+import { Minimalist } from '../../lib/Utility/Minimalist.js';
 
 const range = Range(0, 7, true);
+const processArgs = new Minimalist(process.argv.slice(2).join(' '));
 
 @RegisterCommand
 export class kCommand extends Command {
@@ -19,7 +23,10 @@ export class kCommand extends Command {
                 'Ban a member from the guild.',
                 '@user 3d for a good reason',
                 '@user 0 bye!',
-                '239566240987742220 7d'
+                '239566240987742220 7d',
+                '@user --days 7 --reason he was being toxic',
+                '@user --reason goodbye sir! --days 3',
+                '@user --days 2 this is the reason.'
             ],
 			{
                 name: 'ban', 
@@ -32,9 +39,10 @@ export class kCommand extends Command {
         );
     }
 
-    async init(message: Message, { args }: Arguments) {
+    async init(message: Message, { args, cli }: Arguments) {
+        // the user might not be in the guild, but we still need to ban them
+        // so we fetch their user object rather than a possibly non-existent member
         const user = await getMentions(message, 'users');
-        const clear = typeof args[1] === 'string' ? Math.ceil(ms(args[1]) / 86400000) : 7;
 
         const member = message.guild.members.resolve(user);
         if (member && !hierarchy(message.member, member)) {
@@ -43,22 +51,72 @@ export class kCommand extends Command {
             return this.Embed.fail(`No user id or user mentioned, no one was banned.`);
         }
 
-        const reason = args.slice(args[1] && ms(args[1]) ? 2 : 1).join(' ');
-        try {
-            await message.guild.members.ban(user, {
-                days: range.isInRange(clear) && validateNumber(clear) ? clear : 7,
-                reason: reason.length > 0 ? reason : `Requested by ${message.member.id}`
-            });
-            
-            if (!bans.has(`${message.guild.id},${user.id}`)) // not in the cache already, just to be sure
-                bans.set(`${message.guild.id},${user.id}`, { member: message.member, reason });
-        } catch {
-            return this.Embed.fail(`${user} isn't bannable!`);
+        // days of messages to clear
+        let clear = 7, usedMs = false;
+
+        if (cli.has('days') || cli.has('time')) {
+            const arg = cli.get('days') || cli.get('time');
+            const time = Array.isArray(arg) ? Number(arg[0]) : Number(arg);
+
+            if (validateNumber(time) && range.isInRange(time)) {
+                clear = time;
+            }
+        } else if (typeof args[1] === 'string') {
+            const time = Math.ceil(ms(args[1]) / 86_400_000); // ms -> days
+
+            if (validateNumber(time) && range.isInRange(time)) {
+                clear = time;
+                usedMs = true;
+            }
         }
 
+        let reason = `Requested by ${message.author.tag} (${message.author.id}).`;
+
+        if (cli.has('reason') || cli.has('r')) {
+            const str = cli.get('reason') || cli.get('r');
+
+            if (typeof str === 'string' || Array.isArray(str)) {
+                reason = typeof str === 'string'
+                    ? str
+                    : str.join(' ');
+            }
+        } else if (usedMs) {
+            // ban @user 3d reason here -> reason here
+            // ban @user reason here -> reason here
+            reason = args.slice(usedMs ? 2 : 1).join(' ');
+        } else {
+            const idx = args.findIndex(a => a.startsWith('--time') || a.startsWith('--days'));
+            let r = '';
+
+            if (args.slice(idx + 2).length === 0 && idx !== -1) {
+                r = args.slice(1, idx).join(' '); // ban @user reason here --days 3 -> reason here
+            } else {
+                r = args.slice(idx + 2).join(' '); // ban @user --days 3 reason here -> reason here
+            }
+
+            if (r !== '') {
+                reason = r;
+            }
+        }
+
+        if (!processArgs.get('dev')) {
+            const [err] = await dontThrow(message.guild.members.ban(user, {
+                days: clear,
+                reason: reason
+            }));
+
+            if (err !== null) {
+                return this.Embed.fail(`${member ?? user} is not bannable!`);
+            }
+        }
+
+        if (!bans.has(`${message.guild.id},${user.id}`)) // not in the cache already, just to be sure
+            bans.set(`${message.guild.id},${user.id}`, { member: message.member, reason });
+
         return this.Embed.success(`
-        ${user} has been banned from the guild and ${Number.isNaN(clear) ? '7' : clear}` +
-        ` days worth of messages have been removed.
-        `);
+        ${member ?? user} has been banned from the guild for ${inlineCode(reason)}!
+        `).setFooter(
+            `${clear} days of messages removed.`
+        );
     }
 }
