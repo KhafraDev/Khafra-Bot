f33c1603bfa814a70a9706baafbda8ac0be8ca78
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-09-17 20:08:54 -0400

    improve `clear` command
    - more descriptive, accurate error messages
    - remove the command message before clearing, allowing for users to properly clear 100 messages.
    - proper permission checks for channel
    - properly fetch channel
    - use `dontThrow` utility function

--- src/Commands/Moderation/Clear.ts
+++ src/Commands/Moderation/Clear.ts
@@ -4,6 +4,10 @@ import { RegisterCommand } from '../../Structures/Decorator.js';
 import { isText } from '../../lib/types/Discord.js.js';
 import { Range } from '../../lib/Utility/Range.js';
 import { validateNumber } from '../../lib/Utility/Valid/Number.js';
+import { hasPerms } from '../../lib/Utility/Permissions.js';
+import { getMentions } from '../../lib/Utility/Mentions.js';
+import { dontThrow } from '../../lib/Utility/Don\'tThrow.js';
+import { inlineCode } from '@discordjs/builders';
 
 const range = Range(1, 100, true);
 
@@ -27,20 +31,31 @@ export class kCommand extends Command {
     }
 
     async init(message: Message, { args }: Arguments) {
-        const toDelete = Number(args.shift()) + 1;
-        if (!range.isInRange(toDelete) || !validateNumber(toDelete)) {
+        const toDelete = Number(args[0]);
+
+        if (!validateNumber(toDelete)) {
             return this.Embed.fail(`
             Received: ${toDelete}, this command requires a valid integer!
 
             Example: \`\`${this.settings.name} 100\`\`
             `);
+        } else if (!range.isInRange(toDelete)) {
+            return this.Embed.fail(`${toDelete.toLocaleString()} is not within the range of 0-100 messages!`);
         }
 
-        const channel = message.mentions.channels.size > 0 ? message.mentions.channels.first() : message.channel;
-        if (!isText(channel))
+        const channel = await getMentions(message, 'channels', { idx: 1 }) ?? message.channel;
+        
+        if (!isText(channel) || !hasPerms(channel, message.guild!.me, [Permissions.FLAGS.MANAGE_MESSAGES])) {
             return this.Embed.fail('Can\'t delete messages from this type of channel, sorry!');
+        } else if (message.deletable) {
+            await dontThrow(message.delete());
+        }
 
-        const deleted = await channel.bulkDelete(toDelete > 100 ? 100 : toDelete, true);
+        const [err, deleted] = await dontThrow(channel.bulkDelete(toDelete, true));
+
+        if (err !== null) {
+            return this.Embed.fail(`An unexpected error occurred: ${inlineCode(err.message)}.`);
+        }
 
         const embed = this.Embed.success()
             .setAuthor(message.client.user!.username, message.client.user!.displayAvatarURL())
@@ -49,12 +64,12 @@ export class kCommand extends Command {
             .setDescription(`
             Successfully deleted ${deleted.size} messages!
 
-            \`\`If this number isn't correct, it is because messages older than 2 weeks cannot be cleared and your input message has also been deleted.\`\`
+            If this number isn't correct, it is because messages older than 2 weeks cannot be deleted!
             `);
 
-        const m = await message.reply({ embeds: [embed] });
+        const [, m] = await dontThrow(message.reply({ embeds: [embed] }));
         setTimeout(() => {
-            if (m.deletable) void m.delete();
+            if (m?.deletable) void dontThrow(m.delete());
         }, 5000).unref();
     }
 }
