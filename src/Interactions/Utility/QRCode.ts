2da40ae3edd37c74511d4db38ecbd933bd29e2e0
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-09-22 11:15:36 -0400

    add husky+qr interaction

---
+++ src/Interactions/Utility/QRCode.ts
@@ -0,0 +1,40 @@
+import { CommandInteraction, InteractionReplyOptions, MessageAttachment } from 'discord.js';
+import { Interactions } from '../../Structures/Interaction.js';
+import { inlineCode, SlashCommandBuilder } from '@discordjs/builders';
+import { fetch } from 'undici';
+import { dontThrow } from '../../lib/Utility/Don\'tThrow.js';
+
+export class kInteraction extends Interactions {
+    constructor() {
+        const sc = new SlashCommandBuilder()
+            .setName('qr')
+            .addStringOption(option => option
+                .setName('input')
+                .setDescription('text to get a QR code for')
+                .setRequired(true)
+            )
+            .setDescription('Get the QR code for some text.');
+
+        super(sc, { defer: true });
+    }
+
+    async init(interaction: CommandInteraction) {
+        const text = interaction.options.getString('input', true);
+        const [e, r] = await dontThrow(fetch(`https://qrcode.show/${text}`, {
+            headers: {
+                Accept: 'image/png'
+            }
+        }));
+
+        if (e !== null) {
+            return `❌ An unexpected error occurred: ${inlineCode(e.message)}.`;
+        }
+
+        const qr = Buffer.from(await r.arrayBuffer());
+        const attachment = new MessageAttachment(qr, 'qr.png');
+
+        return {
+            files: [attachment]
+        } as InteractionReplyOptions;
+    }
+} 
