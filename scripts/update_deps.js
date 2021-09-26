c2ce5938a43beb0ff364771549393b3cf85adbb6
Khafra <42794878+KhafraDev@users.noreply.github.com>
2021-09-26 17:23:01 -0400

    scripts: fix update deps

--- scripts/update_deps.js
+++ scripts/update_deps.js
@@ -1,9 +1,11 @@
 import { execSync } from 'child_process';
 
+console.log(execSync('npm outdated').toString());
+
 const outdated = JSON.parse(execSync('npm outdated --json').toString());
 
 for (const key of Object.keys(outdated)) {
     const pkg = outdated[key];
-    console.log(`Installing ${key}@${pkg.wanted} (current:${pkg.current})`);
-    execSync(`npm i ${key}@${pkg.wanted}`);
+    console.log(`Installing ${key}@${pkg.latest} (current:${pkg.current})`);
+    execSync(`npm i ${key}@${pkg.latest}`);
 }
