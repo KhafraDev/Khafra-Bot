f2fc75bd1fe40aeb8afec19efd1d8e8db33ae72f
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-01-11 00:05:48 -0500

    remove unused utility function

--- src/lib/Utility/Template.ts
+++ src/lib/Utility/Template.ts
@@ -1,23 +1,4 @@
 /**
- * Trim a multi-line template literal to a single line.
- * @example
- * const a = `
- * Foo: ${1 + 2}
- * Bar: Hello!
- * Baz: ${2 - 20}
- * `;
- * assert.strictEqual(trim`${a}`, 'Foo: 3 Bar: Hello! Baz: -18');
- */
-export const trim = (...s: [TemplateStringsArray, ...(string | number)[]]) => {
-    let str = '';
-    for (let i = 1; i < s.length; i++) {
-        str += s[0][i-1] + (s[i] ?? '');
-    }
-    str += s[0].slice(s.length - 1).join('');
-    return str.split(/\r?\n/g).map(e => e.trim()).join(' ').trim();
-}
-
-/**
  * Strip leading indents from a multi-lined template literal.
  */
 export const stripIndents = (raw: TemplateStringsArray, ...args: unknown[]) => {
