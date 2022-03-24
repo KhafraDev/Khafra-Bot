e246e12c1a67c6739e9c2786eb95f6990f595d19
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-03-23 21:28:58 -0400

    remove esqlite

--- src/lib/Utility/Rejections.ts
+++ src/lib/Utility/Rejections.ts
@@ -1,6 +1,5 @@
 import { client as DiscordClient } from '#khaf/Client';
 import { sql as PostgresClient } from '#khaf/database/Postgres.js';
-import { db as SQLiteClient } from '#khaf/database/SQLite.js';
 import { logger } from '#khaf/Logger';
 import Graceful from 'node-graceful';
 import { exit } from 'process';
@@ -12,7 +11,6 @@ Graceful.on('exit', async (reason, promise) => {
 
     await PostgresClient.end({ timeout: 5 });
     DiscordClient.destroy();
-    SQLiteClient.close();
     logger.close();
 
     exit(1);
