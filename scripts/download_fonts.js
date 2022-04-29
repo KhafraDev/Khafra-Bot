66917951157dff173941754595532b4bbd8f4b87
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-04-29 16:22:27 -0400

    add files

---
+++ scripts/download_fonts.js
@@ -0,0 +1,37 @@
+import { existsSync, mkdirSync, writeFileSync } from 'fs';
+import { join } from 'path';
+import { request } from 'undici';
+import { fileURLToPath } from 'url';
+
+const fontDir = join(fileURLToPath(import.meta.url), '../../assets/Fonts');
+const fonts = {
+	'AppleColorEmoji.ttf': 'https://github.com/samuelngs/apple-emoji-linux/releases/download/ios-15.4/AppleColorEmoji.ttf',
+	'Arial.ttf': 'https://raw.githubusercontent.com/someblu/Microsoft-Fonts/master/arial.ttf',
+	'Gabriola.ttf': 'https://raw.githubusercontent.com/someblu/Microsoft-Fonts/master/Gabriola.ttf',
+	'Impact.ttf': 'https://raw.githubusercontent.com/KhafraDev/Khafra-Bot/a7cbd1db105c06b690a2a23cac4a5effa3ceee2e/assets/Fonts/Impact.ttf',
+	'Roboto.ttf': 'https://raw.githubusercontent.com/KhafraDev/Khafra-Bot/a7cbd1db105c06b690a2a23cac4a5effa3ceee2e/assets/Fonts/Roboto.ttf'
+};
+
+if (!existsSync(fontDir)) {
+	mkdirSync(fontDir);
+}
+
+for (const [fileName, url] of Object.entries(fonts)) {
+	if (existsSync(join(fontDir, fileName))) {
+		continue;
+	}
+
+	console.log(`downloading ${fileName}...`);
+	const { body, statusCode } = await request(url);
+
+	if (statusCode !== 200) {
+		throw new Error(`unexpected status code while downloading ${fileName}: ${statusCode} (${await body.text()})`);
+	}
+
+	const buffer = Buffer.from(await body.arrayBuffer());
+	console.log(`downloaded ${fileName}`);
+
+	writeFileSync(join(fontDir, fileName), buffer);
+}
+
+console.log('OK. done');
