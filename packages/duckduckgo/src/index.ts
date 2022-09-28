76294a1ab739a8334586bbdbfebbb8bb2c0f38f7
Khafra <42794878+KhafraDev@users.noreply.github.com>
2022-09-28 01:09:18 -0400

    feat: improve youtube search command

---
+++ packages/duckduckgo/src/index.ts
@@ -0,0 +1,45 @@
+/* eslint-disable no-restricted-globals */
+
+const handleEvent = async (request: Request): Promise<Response> => {
+    const { searchParams } = new URL(request.url)
+    let query = searchParams.get('q')
+
+    if (query === null) {
+        return Response.json({
+            error: 'Missing q parameter'
+        }, { status: 400 })
+    } else {
+        query += 'site:youtube.com'
+    }
+
+    const urls: string[] = []
+    const encoded = encodeURIComponent(query.replaceAll(/\s/g, '+'))
+    const response = await fetch(`https://html.duckduckgo.com/html/?q=${encoded}`, {
+        headers: {
+            'user-agent': 'Khafra-Bot (https://github.com/KhafraDev/Khafra-Bot)'
+        }
+    })
+
+    const rewriter = new HTMLRewriter().on('a', {
+        element (element) {
+            const classList = element.getAttribute('class')
+            if (classList === 'result_a' || classList === 'result__snippet') {
+                const href = element.getAttribute('href')
+                const url = new URL(href!, 'https://duckduckgo.com')
+                const uddg = url.searchParams.get('uddg')
+
+                if (uddg !== null) {
+                    urls.push(uddg)
+                }
+            }
+        }
+    })
+
+    await rewriter.transform(response).text()
+
+    return Response.json(urls)
+}
+
+addEventListener('fetch', (event) => {
+    event.respondWith(handleEvent(event.request))
+})
--- src/Interactions/Utility/YouTube.ts
+++ src/Interactions/Utility/YouTube.ts
@@ -1,31 +1,23 @@
 import { Interactions } from '#khaf/Interaction'
-import { YouTube, type YouTubeSearchResults } from '#khaf/utility/commands/YouTube'
 import { Buttons, Components, disableAll } from '#khaf/utility/Constants/Components.js'
-import { colors, Embed } from '#khaf/utility/Constants/Embeds.js'
-import { bold, time } from '@discordjs/builders'
-import type { APIEmbed } from 'discord-api-types/v10'
-import { ApplicationCommandOptionType, InteractionType, type RESTPostAPIApplicationCommandsJSONBody } from 'discord-api-types/v10'
+import { s } from '@sapphire/shapeshift'
 import {
-    InteractionCollector, type ButtonInteraction, type ChatInputCommandInteraction, type InteractionReplyOptions
+    ApplicationCommandOptionType,
+    InteractionType,
+    type RESTPostAPIApplicationCommandsJSONBody
+} from 'discord-api-types/v10'
+import {
+    InteractionCollector,
+    type ButtonInteraction,
+    type ChatInputCommandInteraction,
+    type InteractionReplyOptions
 } from 'discord.js'
 import { randomUUID } from 'node:crypto'
+import { URL } from 'node:url'
+import { request } from 'undici'
 
-function * format({ items }: YouTubeSearchResults): Generator<APIEmbed, void, unknown> {
-    for (let i = 0; i < items.length; i++) {
-        const video = items[i].snippet
-        yield Embed.json({
-            color: colors.ok,
-            description: `${video.description.slice(0, 2048)}`,
-            title: video.title,
-            author: { name: video.channelTitle },
-            thumbnail: { url: video.thumbnails.default.url },
-            fields: [
-                { name: bold('Published:'), value: time(new Date(video.publishTime)) },
-                { name: bold('URL:'), value: `https://youtube.com/watch?v=${items[i].id.videoId}` }
-            ]
-        })
-    }
-}
+const base = 'https://duckduckgo.khafra.workers.dev'
+const schema = s.string.array.lengthGreaterThan(0)
 
 export class kInteraction extends Interactions {
     constructor () {
@@ -47,26 +39,32 @@ export class kInteraction extends Interactions {
 
     async init (interaction: ChatInputCommandInteraction): Promise<InteractionReplyOptions | undefined> {
         const query = interaction.options.getString('search', true)
-        const results = await YouTube(query)
+            .replaceAll(':', '-')
 
-        if ('error' in results) {
+        const url = new URL(base)
+        url.searchParams.set('q', query)
+
+        const { body } = await request(url)
+        const result: unknown = await body.json()
+
+        if (!!result && typeof result === 'object' && 'error' in result) {
             return {
-                content: `❌ ${results.error.code}: ${results.error.message}`,
-                ephemeral: true
-            }
-        } else if (results.pageInfo.totalResults === 0 || results.items.length === 0) {
-            return {
-                content: '❌ No results found!',
+                content: 'An unexpected error occurred, sorry! :(',
                 ephemeral: true
             }
         }
 
-        const id = randomUUID()
-        const embeds = [...format(results)]
-        let page = 0
+        if (!schema.is(result)) {
+            return {
+                content: 'No results found!',
+                ephemeral: true
+            }
+        }
 
-        const m = await interaction.editReply({
-            embeds: [embeds[0]],
+        let currentPage = 0
+        const id = randomUUID()
+        const int = await interaction.editReply({
+            content: result[currentPage],
             components: [
                 Components.actionRow([
                     Buttons.approve('Next', `next-${id}`),
@@ -76,36 +74,43 @@ export class kInteraction extends Interactions {
             ]
         })
 
-        const c = new InteractionCollector<ButtonInteraction>(interaction.client, {
+        const collector = new InteractionCollector<ButtonInteraction>(interaction.client, {
             interactionType: InteractionType.MessageComponent,
-            message: m,
-            time: 120_000,
-            idle: 60_000,
+            message: int,
+            idle: 120_000,
             filter: (i) =>
+                i.message.id === int.id &&
                 i.user.id === interaction.user.id &&
                 i.customId.endsWith(id)
         })
 
-        for await (const [i] of c) {
-            if (i.customId.startsWith('stop-') || c.total >= embeds.length) {
-                c.stop()
-                break
-            }
+        for await (const [collected] of collector) {
+            const [action] = collected.customId.split('-')
 
-            i.customId.startsWith('next-') ? page++ : page--
+            if (action === 'stop') break
 
-            if (page < 0) page = embeds.length - 1
-            if (page >= embeds.length) page = 0
+            action === 'next' ? currentPage++ : currentPage--
+            if (currentPage < 0) currentPage = result.length - 1
+            if (currentPage >= result.length) currentPage = 0
 
-            await i.update({ embeds: [embeds[page]] })
+            await collected.update({
+                content: result[currentPage]
+            })
         }
 
-        if (c.collected.size !== 0) {
-            const last = c.collected.last()
+        const last = collector.collected.last()
 
-            if (last) {
-                await last.update({ components: disableAll(m) })
-            }
+        if (
+            collector.collected.size !== 0 &&
+            last?.replied === false
+        ) {
+            return void await last.update({
+                components: disableAll(int)
+            })
         }
+
+        return void await interaction.editReply({
+            components: disableAll(int)
+        })
     }
 }
